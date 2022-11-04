c9e6f9eca50576127aba23bac12faf5e0a226231
oet <54170438+ohet9409@users.noreply.github.com>
2022-11-04 19:15:40 +0900

    부트스트랩 적용

---
+++ learn-vue3/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <the-nav></the-nav>
+    <the-view></the-view>
+  </div>
+</template>
+
+<script>
+import TheNav from "./components/TheNav.vue";
+import TheView from "./components/TheView.vue";
+export default {
+  components: { TheNav, TheView },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/components/TheView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <main>
+      <div class="container py-4">
+        <div class="row g-3">
+          <div class="col-4 col" v-for="post in posts" :key="post.id">
+            <app-card
+              :title="post.title"
+              :contents="post.contents"
+              :type="post.type"
+              :is-like="post.isLike"
+            ></app-card>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+import AppCard from "./AppCard.vue";
+export default {
+  components: { AppCard },
+  setup() {
+    const post = reactive({
+      title: "제목2",
+      contents: "내용2",
+    });
+
+    const posts = reactive([
+      { id: 1, title: "제목1", contents: "내용1", isLike: true, type: "news" },
+      { id: 2, title: "제목2", contents: "내용2", isLike: true, type: "news" },
+      { id: 3, title: "제목3", contents: "내용3", isLike: true, type: "news" },
+      {
+        id: 4,
+        title: "제목4",
+        contents: "내용4",
+        isLike: false,
+        type: "notice",
+      },
+      {
+        id: 5,
+        title: "제목5",
+        contents: "내용5",
+        isLike: false,
+        type: "notice",
+      },
+    ]);
+
+    return { post, posts };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
