9e25c8a4daaae8c1c5672c815bbd94f36f91e01f
oet <dhdmsxor1@cj.net>
2023-01-10 19:17:27 +0900

    템플릿 참조 ref, parents

--- learn-vue3/src/components/TheView.vue
+++ learn-vue3/src/components/TheView.vue
@@ -3,18 +3,21 @@
     <!-- <div class="container py-4">
       <ProvideInject></ProvideInject>
     </div> -->
-    <LifeCycleHook />
+    <!-- <LifeCycleHook /> -->
+    <TemplateRefs />
   </main>
 </template>
 
 <script>
 import LifeCycleHook from "./LifeCycleHook.vue";
+import TemplateRefs from "./TemplateRefs.vue";
 
 // import ProvideInject from "./ProvideInject.vue";
 export default {
   components: {
     // ProvideInject,
     LifeCycleHook,
+    TemplateRefs,
   },
   setup() {
     return {};
